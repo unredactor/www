3087399e91b26d5f27115dab818e98689b2f2a1d
hobs <hobs+github@totalgood.com>
2019-08-10 13:11:51 -0700

    swap out the .git for unredactor/www .git

--- unredactor/app/routes.py
+++ unredactor/app/routes.py
@@ -1,9 +1,14 @@
-from flask import request
-from flask import render_template
+from flask import render_template, flash, redirect, request, url_for
 from app import app
 
 from constants import context
 from nlp import sort_words
+from app.forms import UnredactForm
+#from muellerbot import unredact
+from unredactor_functions import unredact
+
+# def sort_words(text):
+#     return ' '.join(sorted(text.split()))
 
 
 @app.route('/')
@@ -17,8 +22,20 @@ def about():
     return render_template('about.html', **context)
 
 
-@app.route('/unredact')
-def unredact():
+@app.route('/api')
+def api():
     text = request.args.get('text')
     unredacted_text = sort_words(text)
     return render_template('unredacted_text.json', text=text, unredacted_text=unredacted_text)
+
+@app.route('/unredactor', methods=['GET', 'POST'])
+def unredactor():
+	form = UnredactForm()
+	unredacted_text = ''
+
+	#Depending on which module is imported, either the sort function (unredactor functions) or
+	#the actual unredact function (muellerbot) runs	
+
+	if form.validate_on_submit():
+		unredacted_text = unredact(str(form.text.data))
+	return render_template('unredact.html', title='Unredact', form=form, unredacted_text=unredacted_text)
